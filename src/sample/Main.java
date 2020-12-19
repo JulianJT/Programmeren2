14ecf57ef284d19def42baa4db17ec4e878bd205
mhaddouch <74668420+mhaddouch@users.noreply.github.com>
2020-12-19 12:16:11 +0100

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -37,7 +37,7 @@ public class Main extends Application {
     public static void main(String[] args) {
 
         launch(Main.class);
-        
+                                    //hopelijk veranderd dit
         String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=Bibliotheek;user=sa;password=12345;portNumber=1433\n;";
 
         // Connection beheert informatie over de connectie met de database.
