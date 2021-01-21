aad8939587e96e1bd999e32a596a3fb73120a2da
mhaddouch <74668420+mhaddouch@users.noreply.github.com>
2021-01-21 21:57:57 +0100

    course

---
+++ src/Domain/Course.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class Course {
+    private String name;
+    private String subject;
+    private String text;
+
+    public Course(String name, String subject, String text) {
+        this.name=name;
+        this.subject=subject;
+        this.text=text;
+    }
+
+}
