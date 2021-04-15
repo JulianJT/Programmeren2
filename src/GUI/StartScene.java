b728f45f96a6383444b4c576af6fd35eea6d4228
JulianJT <j.d.tjintjelaar@gmail.com>
2021-04-15 15:01:47 +0200

    UI change

--- src/GUI/StartScene.java
+++ src/GUI/StartScene.java
@@ -1,38 +1,61 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class StartScene extends Application {
-    private static GridPane getStartPane;
+import javafx.scene.image.Image;
+import java.io.FileInputStream;
 
+public class StartScene extends Application {
+
+
+
+    private static HBox root2;
     public static Pane getStartMenu() {
-        return getStartPane;
+        return root2;
     }
 
+    // C:\Users\jdtji\Desktop\eo_codecademy_01.jpg
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        getStartPane = new GridPane();
-        getStartPane.setVgap(8);
-        getStartPane.setHgap(10);
+        Image image1 = new Image("eo_codecademy_01.jpg");
+
+        ImageView imageView = new ImageView(image1);
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(100);
+        VBox root = new VBox();
+
+        root2 = new HBox();
+
+        root.setSpacing(10);
 
         Button student = new Button("Students");
+        student.setMinWidth(250);
         Button course = new Button("Courses");
+        course.setMaxSize(500,200);
         Button registration = new Button("Registrations");
+        registration.setMaxSize(500,200);
         Button certificate = new Button("Certificates");
+        certificate.setMaxSize(500,200);
 
-        getStartPane.add(student, 1, 1);
-        getStartPane.add(course, 1, 2);
-        getStartPane.add(registration, 1, 3);
-        getStartPane.add(certificate, 1, 4);
+        root.getChildren().addAll(imageView, student, course, registration, certificate);
 
+        root2.getChildren().add(root);
 
-        Scene view = new Scene(getStartPane, 500, 300);
+        root2.setAlignment(Pos.CENTER);
+        root.setAlignment(Pos.CENTER);
+
+        Scene view = new Scene(root2, 500, 300);
 
         primaryStage.setTitle("Julian Tjintjelaar (2173067) | Sten Reijerse (2175709) | Wesley Snijdelaar (2176710) | Mohamed Haddouch (2177710)");
         primaryStage.setScene(view);
