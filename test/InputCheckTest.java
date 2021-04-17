c36416c27aa94e7b7713abf71e3f97f72f5ab1cd
Wesley1808 <wesleysnijdelaar@outlook.com>
2021-04-17 17:22:20 +0200

    unit test update

--- test/InputCheckTest.java
+++ test/InputCheckTest.java
@@ -1,32 +1,56 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InputCheckTest {
 
     @Test
-    @DisplayName("Certificate: Non-integers")
+    @DisplayName("AddCertificate: Non-integers")
     public void testCertificateInput() {
         boolean bl = addCertificateInputCheck("course", "user", "student", "four");
         assertTrue(bl);
     }
 
     @Test
-    @DisplayName("Certificate: Integers not [1-10]")
+    @DisplayName("AddCertificate: Integers not [1-10]")
     public void testCertificateInput2() {
         boolean bl = addCertificateInputCheck("course", "user", "student", "0");
         assertTrue(bl);
     }
 
     @Test
-    @DisplayName("Certificate: Correct Input")
+    @DisplayName("AddCertificate: Correct Input")
     public void testCertificateInput3() {
         boolean bl = addCertificateInputCheck("course", "user", "student", "7");
         assertFalse(bl);
     }
 
-    // certificateInputCheck without Alerts
+    @Test
+    @DisplayName("Student: Incorrect Email")
+    public void testStudentInput1() {
+        boolean bl = addStudentInputCheck("user", "notEmail", "address", "residence", "country");
+        assertTrue(bl);
+    }
+
+    @Test
+    @DisplayName("Student: Empty Field")
+    public void testStudentInput3() {
+        boolean bl = addStudentInputCheck("", "student@outlook.com", "address", "residence", "country");
+        assertTrue(bl);
+    }
+
+    @Test
+    @DisplayName("Student: Correct Input")
+    public void testStudentInput2() {
+        boolean bl = addStudentInputCheck("user", "student@outlook.com", "address", "residence", "country");
+        assertFalse(bl);
+    }
+
+    // addCertificateInputCheck without Alert Dialogs.
     public boolean addCertificateInputCheck(String course, String userName, String studentName, String review) {
         if (course.isEmpty()) {
             return true;
@@ -47,6 +71,40 @@ public class InputCheckTest {
         if (!review.isEmpty()) {
             return Integer.parseInt(review) < 1 || Integer.parseInt(review) > 10;
         }
+
+        return false;
+    }
+
+    // addStudentInputCheck without Alert Dialogs.
+    public boolean addStudentInputCheck(String userName, String email, String address, String residence, String country) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (!(matcher.matches())) {
+            return true;
+        }
+
+        if (userName.isEmpty()) {
+            return true;
+        }
+
+        if (email.isEmpty()) {
+            return true;
+        }
+
+        if (address.isEmpty()) {
+            return true;
+        }
+
+        if (residence.isEmpty()) {
+            return true;
+        }
+
+        if (country.isEmpty()) {
+            return true;
+        }
+
         return false;
     }
 
